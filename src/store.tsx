1dd2f521ca41dd0018e3583b5fc56e1d624f67e9
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-04 15:59:53 +0300

    Added the middleware to the store

--- src/store.tsx
+++ src/store.tsx
@@ -6,4 +6,5 @@ export const store = configureStore({
     reducer: {
         [contactsApi.reducerPath]: contactsApi.reducer
     },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(contactsApi.middleware)
 })

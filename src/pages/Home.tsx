ec1fa97e68525370609a28cb78036ba25299b639
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-04 16:08:00 +0300

    Destructured data, isLoading and error

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,10 +2,13 @@ import { useEffect } from 'react'
 import './Home.css'
 import { Link } from 'react-router-dom'
 import { toast } from 'react-toastify'
+import { useContactsQuery } from '../services/contactsApi'
 
 
 const Home = () =>
 {
+    const { data, isLoading, error } = useContactsQuery()
+
     return (
         <div>Home</div>
     )

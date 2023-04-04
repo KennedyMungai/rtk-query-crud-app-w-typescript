500b00a8d1a59d1dfb01f7f610b19a27b36a08a9
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-04 16:18:56 +0300

    Finished writing the logic for the contacts table

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -25,13 +25,39 @@ const Home = () =>
             <table className="styled-table">
                 <thead>
                     <tr>
-                        <th style={{ textAlign: 'center' }}>ID</th>
+                        <th style={{ textAlign: 'center' }}>No.</th>
                         <th style={{ textAlign: 'center' }}>Name</th>
                         <th style={{ textAlign: 'center' }}>Email</th>
                         <th style={{ textAlign: 'center' }}>Contact</th>
                         <th style={{ textAlign: 'center' }}>Action</th>
                     </tr>
                 </thead>
+                <tbody>
+                    {data?.map((item: any, index: any) =>
+                    {
+                        return (
+                            <tr key={item.id}>
+                                <th scope='row'>{index + 1}</th>
+                                <td>{item.name}</td>
+                                <td>{item.email}</td>
+                                <td>{item.contact}</td>
+                                <td>
+                                    <Link to={`/editContact/${item.id}`}>
+                                        <button className="btn btn-edit">Edit</button>
+                                    </Link>
+                                    <button className="btn btn-delete">
+                                        Delete
+                                    </button>
+                                    <Link to={`/info/${item.id}`}>
+                                        <button className="btn btn-v">
+                                            View
+                                        </button>
+                                    </Link>
+                                </td>
+                            </tr>
+                        )
+                    })}
+                </tbody>
             </table>
         </div>
     )

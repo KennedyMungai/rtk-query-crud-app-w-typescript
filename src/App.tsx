03d2bfd69b7e127d1ba0cd8789b137a1e22bb4b3
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-04 15:38:58 +0300

    Added some more routes to the web app

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
+import AddEdit from './pages/AddEdit';
+import Info from './pages/Info';
 
 
 function App()
@@ -9,6 +11,9 @@ function App()
     <Router>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/addContact' element={<AddEdit />} />
+        <Route path='/editContact/:id' element={<AddEdit />} />
+        <Route path='/info/:id' element={<Info />} />
       </Routes>
     </Router>
   );

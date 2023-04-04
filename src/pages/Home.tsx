937ad60572fe1e984599cd7bf62c76b04a1fabbe
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-04 16:06:30 +0300

    Got rid of the props and the props type

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,9 +4,7 @@ import { Link } from 'react-router-dom'
 import { toast } from 'react-toastify'
 
 
-type Props = {}
-
-const Home = (props: Props) =>
+const Home = () =>
 {
     return (
         <div>Home</div>

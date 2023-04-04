f8584db8efd5be3273a2011ee3457e4fd3f25e24
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-04 15:48:13 +0300

    Wrote a bunch of code

--- src/services/contactsApi.tsx
+++ src/services/contactsApi.tsx
@@ -1,2 +1,13 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { Contact } from '../model/contact.model'
+
+
+export const contactsApi = createApi({
+    reducerPath: "contactsApi",
+    baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5000' }),
+    endpoints: (builder) => ({
+        contacts: builder.query<Contact[], void>({
+            query: () => "/contacts",
+        })
+    })
+})

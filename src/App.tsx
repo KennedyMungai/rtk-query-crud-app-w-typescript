a871104cf95ad40e7ff2efb82947a2c506c2d364
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-04 15:40:47 +0300

    Spaced out some of my imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,9 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import
+  {
+    BrowserRouter as Router,
+    Route,
+    Routes
+  } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
 import AddEdit from './pages/AddEdit';

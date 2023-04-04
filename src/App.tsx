7c5fbfd0defc2a99edc24e8601652c12465b85f3
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-04 16:30:39 +0300

    Added te toast container to the app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
 import
-  {
-    BrowserRouter as Router,
-    Route,
-    Routes
-  } from 'react-router-dom';
+{
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
 import AddEdit from './pages/AddEdit';
 import Info from './pages/Info';
+import { ToastContainer } from 'react-toastify';
 
 
 function App()
 {
   return (
     <Router>
+      <ToastContainer />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/addContact' element={<AddEdit />} />

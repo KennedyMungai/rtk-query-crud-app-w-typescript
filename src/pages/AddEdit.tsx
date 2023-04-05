5069a4c0d87d8e6f3d44bc488c44e819684255d7
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-05 11:16:38 +0300

    Imported the toast from react -toastify

--- src/pages/AddEdit.tsx
+++ src/pages/AddEdit.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import './AddEdit.css'
 import { useNavigate, useParams } from 'react-router-dom'
+import { toast } from 'react-toastify'
 
 
 type Props = {}

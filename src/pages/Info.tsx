79973e04a942af4615d056d4c774a4d7414abebf
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-05 12:15:08 +0300

    Destructured data and error form useContactQuery

--- src/pages/Info.tsx
+++ src/pages/Info.tsx
@@ -2,11 +2,13 @@ import React, { useEffect, useState } from 'react'
 import './Info.css'
 import { Link, useParams } from 'react-router-dom'
 import { toast } from 'react-toastify'
+import { useContactQuery } from '../services/contactsApi'
 
 
 const Info = () =>
 {
     const { id } = useParams()
+    const { data, error } = useContactQuery(id!)
 
     return (
         <div>Info</div>

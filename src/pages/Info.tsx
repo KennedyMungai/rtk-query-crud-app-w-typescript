6ded4e643e9081183a8cdb7898822ef67862b78f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-05 12:11:15 +0300

    Imported toast from react-toastify

--- src/pages/Info.tsx
+++ src/pages/Info.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import './Info.css'
 import { Link, useParams } from 'react-router-dom'
+import { toast } from 'react-toastify'
 
 
 const Info = () =>
